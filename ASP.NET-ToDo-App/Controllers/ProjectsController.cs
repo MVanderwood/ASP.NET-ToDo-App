f02b71983f1b8c355525b8e11b181b3937260a5f
Michael Vanderwood <m.b.vanderwood@gmail.com>
2016-08-27 18:53:24 -0700

    Add ProjectsController.

---
+++ ASP.NET-ToDo-App/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ASP.NET_ToDo_App.Models;
+
+
+namespace ASP.NET_ToDo_App.Controllers
+{
+    [RoutePrefix("projects")]
+    public class ProjectsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
