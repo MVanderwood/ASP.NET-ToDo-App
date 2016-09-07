2475d44a92650e89c347ed91a076cb36bafd1de6
Michael Vanderwood <m.b.vanderwood@gmail.com>
2016-09-06 18:20:10 -0700

    Update seed method in db configuration.

--- ASP.NET-ToDo-App/Migrations/Configuration.cs
+++ ASP.NET-ToDo-App/Migrations/Configuration.cs
@@ -17,18 +17,6 @@
 
         protected override void Seed(Contexts.AppContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
             string[] descriptions = new string[3]
             {
                 "Building a super fancy website",
@@ -115,6 +103,7 @@
                     ProjectID = 3
                 }
             );
+            base.Seed(context);
         }
     }
 }
